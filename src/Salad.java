1ce8588029c4573affd0725f3bf5a7a148114658
user-Sh1111 <balgatai.sh@gmail.com>
2025-01-29 21:55:20 +0500

    Update Salad.java

--- src/Salad.java
+++ src/Salad.java
@@ -1,14 +1,1 @@
 
-public class Salad {
-    String vegitable;
-    double n;
-    double calories;
-
-    public Salad(String vagitable, double n, double calories){
-        this.vegitable = vagitable;
-        this.n = n;
-        this.calories = calories;
-    }
-}
-
-
